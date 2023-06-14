da03796578930351aedfdf48b4b0f6ec93cf04d9
Christoph <40461611+chrisihetti@users.noreply.github.com>
2023-06-14 18:28:10 +0200

    add Procfile

--- src/main/java/org/example/WebhookController.java
+++ src/main/java/org/example/WebhookController.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,4 +17,5 @@ public class WebhookController {
         // Optionally, you can return a response to Jira if required
         return ResponseEntity.ok().build();
     }
+
 }
