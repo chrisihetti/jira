2ed3258f125ded393d8fa5fbc2c15d649d2c90df
Christoph <40461611+chrisihetti@users.noreply.github.com>
2023-06-14 17:42:12 +0200

    make it better

---
+++ src/main/java/org/example/WebhookController.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebhookController {
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(@RequestBody String payload) {
+        // Process the webhook payload received from Jira
+        // Implement your custom logic here
+
+        // Optionally, you can return a response to Jira if required
+        return ResponseEntity.ok().build();
+    }
+}
